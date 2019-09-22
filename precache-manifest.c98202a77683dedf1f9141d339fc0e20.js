555a85815c0f539daf763d830063c24e8c5c0780
unknown <ryu923can@gmail.com>
2019-09-22 12:12:54 -0400

    Updates

---
+++ precache-manifest.c98202a77683dedf1f9141d339fc0e20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0476a0723ac708652b0d268881a71b",
+    "url": "/estore/index.html"
+  },
+  {
+    "revision": "5261f86d9d49ac187063",
+    "url": "/estore/static/css/main.40da7cdc.chunk.css"
+  },
+  {
+    "revision": "6b95c52ff55c85d674ab",
+    "url": "/estore/static/js/2.0c07b0b7.chunk.js"
+  },
+  {
+    "revision": "5261f86d9d49ac187063",
+    "url": "/estore/static/js/main.73d14d74.chunk.js"
+  },
+  {
+    "revision": "00d6816910041de490cc",
+    "url": "/estore/static/js/runtime-main.c32dd43b.js"
+  },
+  {
+    "revision": "c1440ef4dd15c3d66f636836253cf18b",
+    "url": "/estore/static/media/clientSays.c1440ef4.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/estore/static/media/logo.25bf045c.svg"
+  }
+]);
